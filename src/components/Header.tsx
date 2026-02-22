1b32be5201416d5ca64f97bf3e94bd183b83b13d
chasemarshall <git@chasefrazier.dev>
2026-02-21 22:26:39 -0600

    feat: link version badge to roadmap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
         >
           tip jar
         </a>
-        <span className="text-xs text-surface2">v3.0</span>
+        <Link href="/roadmap" className="text-xs text-surface2 hover:text-lavender transition-colors duration-200">v3.0</Link>
       </div>
     </header>
   );
